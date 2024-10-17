bd4bcdfa9aad6304d63904c40ea00d8e3facde38
young219257 <young219257@gmail.com>
2024-10-17 15:26:25 +0900

    init commit

---
+++ src/main/java/com/sparta/course__registeration/CourseRegisterationApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.course__registeration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegisterationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRegisterationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/course__registeration/CourseRegisterationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.course__registeration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegisterationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
