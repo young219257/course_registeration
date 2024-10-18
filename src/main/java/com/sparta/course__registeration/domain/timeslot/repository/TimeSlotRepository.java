bc691fe33ea4d4a227b8ea001f355647193718bc
young219257 <young219257@gmail.com>
2024-10-19 00:29:17 +0900

    [#8] feat : 수업 가능 시간대 조회 기능 추가

--- src/main/java/com/sparta/course__registeration/domain/timeslot/repository/TimeSlotRepository.java
+++ src/main/java/com/sparta/course__registeration/domain/timeslot/repository/TimeSlotRepository.java
@@ -4,6 +4,11 @@ import com.sparta.course__registeration.domain.timeslot.entity.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long> {
+
+    List<TimeSlot> findAllBystartTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
