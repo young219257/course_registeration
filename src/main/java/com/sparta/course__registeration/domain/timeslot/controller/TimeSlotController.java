ba79a787e020f6652c1ce06596f8caedb7dbb95a
young219257 <young219257@gmail.com>
2024-10-19 00:29:11 +0900

    [#8] feat : 수업 가능 시간대 조회 기능 추가

--- src/main/java/com/sparta/course__registeration/domain/timeslot/controller/TimeSlotController.java
+++ src/main/java/com/sparta/course__registeration/domain/timeslot/controller/TimeSlotController.java
@@ -1,12 +1,15 @@
 package com.sparta.course__registeration.domain.timeslot.controller;
 
+import com.sparta.course__registeration.domain.timeslot.dto.AvailableTimeslotRequestDto;
+import com.sparta.course__registeration.domain.timeslot.dto.AvailableTimeslotResponseDto;
 import com.sparta.course__registeration.domain.timeslot.dto.DeleteTimeSlotRequestDto;
 import com.sparta.course__registeration.domain.timeslot.dto.TimeSlotRequestDto;
-import com.sparta.course__registeration.domain.timeslot.entity.TimeSlot;
 import com.sparta.course__registeration.domain.timeslot.service.TimeSlotService;
 import com.sparta.course__registeration.global.exception.handler.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -28,5 +31,14 @@ public class TimeSlotController {
         timeSlotService.deleteTimeSlot(deleteTimeSlotRequestDto);
         return ApiResponse.ok(200,"시간대 삭제 성공");
     }
+
+    //수업 가능 시간대 조회
+    @GetMapping("/api/lessons/timeslots")
+    public ApiResponse<List<AvailableTimeslotResponseDto>> getAvailableTimeSlots(@RequestBody AvailableTimeslotRequestDto availableTimeslotRequestDto) {
+        List<AvailableTimeslotResponseDto> availableTimeslotResponseDtoList=timeSlotService.getAvailableTimeSlots(availableTimeslotRequestDto);
+        return ApiResponse.ok(200,"수업 가능 시간대 조회 성공",availableTimeslotResponseDtoList);
+    }
+
+
 
 }
