f79d50b4b07e5d0aae135e4cabeca7a5f98e6dd4
young219257 <young219257@gmail.com>
2024-10-20 16:09:47 +0900

    [#9] feat : 기간, 수업 길이로 시간대 조회

--- src/main/java/com/sparta/course__registeration/domain/timeslot/repository/TimeSlotRepository.java
+++ src/main/java/com/sparta/course__registeration/domain/timeslot/repository/TimeSlotRepository.java
@@ -1,14 +1,23 @@
 package com.sparta.course__registeration.domain.timeslot.repository;
 
 import com.sparta.course__registeration.domain.timeslot.entity.TimeSlot;
+import com.sparta.course__registeration.domain.tutor.entity.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long> {
 
-    List<TimeSlot> findAllBystartTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
+    List<TimeSlot> findAllByStartTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    Optional<TimeSlot> findByStartTimeAndTutorId(LocalDateTime startTime, Long tutorId);
+
+    Optional<TimeSlot> findByTutorIdAndStartTimeBetween(Long tutorId,LocalDateTime startTime, LocalDateTime endTime);
+
+    boolean existsByTutorAndStartTime(Tutor tutor, LocalDateTime availableTimeSlot);
 }
