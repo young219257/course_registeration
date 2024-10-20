bd2d6fadc5d08d0f8693867ddc2323b0482d5cb4
young219257 <young219257@gmail.com>
2024-10-20 15:58:29 +0900

    [#9] refactor : 출력 형식 변환으로 인한 availableTimeSlot type 변경

--- src/main/java/com/sparta/course__registeration/domain/timeslot/dto/AvailableTimeslotResponseDto.java
+++ src/main/java/com/sparta/course__registeration/domain/timeslot/dto/AvailableTimeslotResponseDto.java
@@ -7,16 +7,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvailableTimeslotResponseDto {
-    private LocalDateTime availableTimeslots;
 
-    public static AvailableTimeslotResponseDto from(TimeSlot timeSlots) {
-        return AvailableTimeslotResponseDto.builder().
-                availableTimeslots(timeSlots.getStartTime()).build();
+    private String availableTimeSlot;
+
+    public static AvailableTimeslotResponseDto from(TimeSlot timeSlot) {
+        // ISO 8601 형식으로 변환
+        String formattedTimeSlot = timeSlot.getStartTime()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")); // 원하는 포맷 지정
+        return AvailableTimeslotResponseDto.builder()
+                .availableTimeSlot(formattedTimeSlot)
+                .build();
     }
 }
+
