3701fadfe909b28b1718d6b6f4b6271e43df0eab
young219257 <young219257@gmail.com>
2024-10-20 16:01:49 +0900

    [#9],[#10] refactor : 수강신청 관련 예외처리 추가, 신청한 수업 조회 관련 코드 리팩토링

--- src/main/java/com/sparta/course__registeration/domain/lesson/service/LessonServiceImpl.java
+++ src/main/java/com/sparta/course__registeration/domain/lesson/service/LessonServiceImpl.java
@@ -10,15 +10,16 @@ import com.sparta.course__registeration.domain.student.entity.Student;
 import com.sparta.course__registeration.domain.student.repository.StudentRepository;
 import com.sparta.course__registeration.domain.timeslot.entity.TimeSlot;
 import com.sparta.course__registeration.domain.timeslot.repository.TimeSlotRepository;
-import com.sparta.course__registeration.domain.tutor.entity.Tutor;
 import com.sparta.course__registeration.global.exception.handler.ErrorCode;
 import com.sparta.course__registeration.global.exception.handler.NotFoundResourceException;
+import com.sparta.course__registeration.global.exception.handler.TimeSlotNotAvailableException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,47 +31,52 @@ public class LessonServiceImpl implements LessonService {
 
 
     @Override
+    @Transactional
     public void signUpLesson(AddLessonRequestDto addLessonRequestDto) {
 
         Student student=findStudentById(addLessonRequestDto.getStudentId());
-        TimeSlot timeSlot=findTimeSlotById(addLessonRequestDto.getTimeSlotId());
+        TimeSlot timeSlot=findTimeSlotByStartTimeAndTutorId(addLessonRequestDto.getTimeSlot(), addLessonRequestDto.getTutorId());
         ClassPath classPath=addLessonRequestDto.getClassPath();
+
+        //이미 예약이 되어있는 시간대의 경우
+        if(!timeSlot.isAvailable()){
+            throw new TimeSlotNotAvailableException(ErrorCode.ALREADY_BOOKING_TIMESLOT);
+        }
 
         Lesson lesson=Lesson.of(classPath, timeSlot.getTutor(), student,timeSlot);
         lessonRepository.save(lesson);
 
-        if(classPath.equals(60)){
+        //예약 완료 상태로 변경
+        timeSlot.updateIsAvailable();
+
+        if(classPath.equals(ClassPath.SIXTY)){
             //timeSlot의 endTime이 startTime이며 tutor가 동일한 TimeSlot을 찾아 lesson에 추가
-            TimeSlot nextTimeSlot = findTimeSlotByStartTimeAndTutor(timeSlot.getEndTime(),timeSlot.getTutor());
+            TimeSlot nextTimeSlot = findTimeSlotByStartTimeAndTutorId(timeSlot.getEndTime(),timeSlot.getTutor().getId());
             Lesson nextLesson =Lesson.of(classPath, nextTimeSlot.getTutor(), student,nextTimeSlot);
             lessonRepository.save(nextLesson);
+
+            //예약 완료 상태로 변경
+            nextTimeSlot.updateIsAvailable();
         }
 
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<LessonResponseDto> getAllLessons(GetLessonsRequestDto getLessonsRequestDto) {
 
         List<Lesson> lessons=lessonRepository.findAllByStudentId(getLessonsRequestDto.getStudentId());
-        List<LessonResponseDto> lessonResponseDtos=new ArrayList<>();
 
-        for(Lesson lesson:lessons){
-
-            lessonResponseDtos.add(LessonResponseDto.from(lesson));
-        }
-
-        return lessonResponseDtos;
+        return lessons.stream()
+                .map(LessonResponseDto::from)
+                .collect(Collectors.toList());
     }
 
     private Student findStudentById(Long studentId) {
         return studentRepository.findById(studentId).orElseThrow(()->new NotFoundResourceException(ErrorCode.NOTFOUND_STUDENT));
     }
 
-    private TimeSlot findTimeSlotById(Long timeSlotId) {
-        return timeSlotRepository.findById(timeSlotId).orElseThrow(()->new NotFoundResourceException(ErrorCode.NOTFOUND_TIMESLOT));
-    }
-
-    private TimeSlot findTimeSlotByStartTimeAndTutor(LocalDateTime startTime, Tutor tutor) {
-        return timeSlotRepository.findBystartTimeAndTutor(startTime,tutor);
+    private TimeSlot findTimeSlotByStartTimeAndTutorId(LocalDateTime startTime, Long tutorId) {
+        return timeSlotRepository.findByTutorIdAndStartTimeBetween(tutorId,startTime,startTime.plusMinutes(30)).orElseThrow(()->new NotFoundResourceException(ErrorCode.NOTFOUND_TIMESLOT));
     }
 }
